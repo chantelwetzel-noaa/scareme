f98e2b3b14b020976f645987f8a00e6ddff1c07e
chantelwetzel-noaa <chantel.wetzel@noaa.gov>
2025-10-18 11:10:35 -0700

    add movies

--- data-raw/create_movie_list.R
+++ data-raw/create_movie_list.R
@@ -1,4 +1,8 @@
-movies <- readr::read_csv("data-raw/movie_list.csv")
+movies <- readr::read_csv("data-raw/movie_list.csv") |>
+  dplyr::group_by(Title) |>
+  dplyr::mutate(
+    Rating = mean(c(`Jason Rating`, `Chantel Rating`), na.rm = TRUE)
+  )
 
 usethis::use_data(
   movies,
